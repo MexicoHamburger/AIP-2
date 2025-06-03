4e08d1752754608df98f75138c93e5a62d902176
Yongjoon Kwon <littlesnail0513@gmail.com>
2025-06-04 06:11:30 +0900

    added react-router & shadcn

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <div className = "bg-gray-100 text-4xl">
+    <div className="bg-gray-100 text-4xl">
       Hello world!
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
     </div>
   )
 }
